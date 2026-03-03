a7ed59507760cc1005978a012e62de90c1a2424d
xyzz-2333 <1793546150@qq.com>
2026-03-03 20:12:00 +0800

    new file:   "leetcode\351\242\230\350\247\243/14.py"

---
+++ leetcode题解/14.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        l=len(strs)
+        c=0
+        a=strs[0]
+        if l==1:
+            return strs[0]
+        while True:
+            for i in strs:
+                if i.startswith(a) ==False:
+                    pass
+                else:
+                    c+=1
+            if c == l:
+                return a
+            else:
+                a = a[:-1]
+                c=0
