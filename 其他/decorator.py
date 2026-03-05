123205b87989af55e7c96657e6986fbc0d18036d
xyzz-2333 <1793546150@qq.com>
2026-03-05 23:11:41 +0800

    modified:   "\345\205\266\344\273\226/decorator.py"

--- 其他/decorator.py
+++ 其他/decorator.py
@@ -15,6 +15,19 @@ def decorator2(func):
         print("调用后")
         return result
     return wrapper
+def timedecorator(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        elapsed = time.time() - start
+        print(f"{func.__name__} 运行时间: {elapsed:.6f} 秒")
+        print(f"运行结果: {result}")
+        return result
+    return wrapper
+@timedecorator
+def sleep(time):
+    time.sleep(time)
 if __name__ == '__main__':
     
     say_hello()
+    sleep(0.5)
