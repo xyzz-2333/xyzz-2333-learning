a8836fe098a16401838b22be9c1d849444b9acb3
xyzz-2333 <1793546150@qq.com>
2026-03-08 23:52:56 +0800

    modified:   "\345\205\266\344\273\226/sorts.py"

--- 其他/sorts.py
+++ 其他/sorts.py
@@ -5,11 +5,22 @@ def SelectSortv1(list=[],visible=1):
         for i in range(min,l):
             if list[i] < list[min]:
                 list[i],list[min]=list[min],list[i]
+        if visible:
+            print(list)
         min+=1
         if min==l:
             return list
 def SelectSortv2(list=[],visible=1):
     l=len(list)
+    for i in range(l):
+        min
+        for j in range(i,l):
+            if list[j]<list[i]:
+                min=list[j]
+                list[i],list[min]=list[min],list[i]
+        if visible:
+            print(list)
+    return list
 if __name__ == '__main__':
     l1=[1,4,7,2,5,8,3,6,9,0]
-    print(SelectSort(l1))
+    print(SelectSortv2(l1))
