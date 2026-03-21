34fcd5d392d45655cb1e34b2596f7b4813437468
xyzz-2333 <1793546150@qq.com>
2026-03-21 23:07:04 +0800

    this is a commit message

--- leetcode题解/26.py
+++ leetcode题解/26.py
@@ -1,7 +1,34 @@
+'''class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        r =needle[0]
+        for i in range(len(haystack)):
+            if len(haystack)-i < len(needle):
+                continue
+            if haystack[i] ==r:
+                for j in range(len(needle)):
+                    flag=0
+                    if haystack[i+j] != needle[j] :
+                        break
+                    else:
+                        flag+=1
+                if flag == len(needle):
+                    return i
+        return -1
+    '''
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        a= sorted(set(nums))
-        l=len(a)        
-        for i in range(l):
-            nums[i] = a[i]
-        return l
+    def strStr(self, haystack: str, needle: str) -> int:
+        r =needle[0]
+        ln,lh=len(needle),len(haystack)
+        for i in range(lh):
+            if i+ln > lh:
+                continue
+            if haystack[i] ==r:
+                flag=0
+                for j in range(ln):  
+                    if haystack[i+j] != needle[j] :
+                        break
+                    else:
+                        flag+=1
+                if flag == ln:
+                    return i
+        return -1
