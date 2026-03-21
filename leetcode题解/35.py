ebbcda861df6f3fe0b9edb65128ba32a4a179803
xyzz-2333 <1793546150@qq.com>
2026-03-21 23:36:40 +0800

    .

---
+++ leetcode题解/35.py
@@ -0,0 +1,8 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l=len(nums)
+        p=l//2
+        while True:
+            if target>p:
+                
+            
