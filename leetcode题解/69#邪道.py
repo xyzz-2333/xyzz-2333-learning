c6f9df5dabbfd5c464dacef8ecc016681e77d757
xyzz-2333 <1793546150@qq.com>
2026-03-25 17:05:48 +0800

    。

---
+++ leetcode题解/69#邪道.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(sqrt(x))
+
+
+
+
+
+'''class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x<1:
+        return 0'''
