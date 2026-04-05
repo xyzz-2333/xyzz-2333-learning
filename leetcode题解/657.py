0b3968d0f9678bded3af80b71f3906aca346ffcd
xyzz-2333 <1793546150@qq.com>
2026-04-05 23:34:05 +0800

    657

---
+++ leetcode题解/657.py
@@ -0,0 +1,16 @@
+#用时: 4m 22s 
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        list=[0,0]
+        for i in range(len(moves)):
+            if moves[i]=='R':
+                list[0]+=1
+            elif moves[i]=='L':
+                list[0]-=1
+            elif moves[i]=='U':
+                list[1]+=1
+            elif moves[i]=='D':
+                list[1]-=1
+        if list==[0,0]:
+            return True
+        else:return False
