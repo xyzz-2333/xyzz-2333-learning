431980e13920f9a80623e5593e01e375419b85e2
xyzz-2333 <1793546150@qq.com>
2026-04-05 23:45:45 +0800

    添加了方法2

--- leetcode题解/657.py
+++ leetcode题解/657.py
@@ -14,3 +14,7 @@ class Solution:
         if list==[0,0]:
             return True
         else:return False
+#更优的方法
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        return moves.count('U') == moves.count('D') and moves.count('L') == moves.count('R')
