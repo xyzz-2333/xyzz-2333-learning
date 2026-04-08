81df5785d3ffcf6e366d521f43a7717f1d259290
xyzz-2333 <1793546150@qq.com>
2026-04-08 20:59:00 +0800

    update 3875 analisis

--- leetcode题解/3875.py
+++ leetcode题解/3875.py
@@ -1,3 +1,10 @@
+#分析：
+'''如果nums1均为奇数或者偶数：nums2[i] = nums1[i]即可
+如果nums1为混合：
+分析1奇1偶的情况：直接奇-偶（如果奇>偶）或偶-奇即可
+当nums1增加新数字时：情况不变
+∴：始终有解
+写什么代码，直接return即可'''
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
         return True
